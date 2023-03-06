d726eff52e80cd2b75d0835b40f0f0d08ae57b09
Cogin <cevinmontgomery05@gmail.com>
2023-03-06 11:16:33 -0600

    Circle + Point2D

--- Circle.java
+++ Circle.java
@@ -1,3 +1,41 @@
+import java.lang.reflect.Constructor;
+
 public class Circle {
-    
+    private Point2D center;
+    public double radius;
+
+    public Circle(Point2D center, double radius){
+        this.center = center;
+        this.radius= radius;
+    }
+
+    public Point2D getCenter() {
+        return center;
+    } 
+
+    public void setCenter(Point2D center) {
+        this.center = center;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public boolean intersects(Circle other) {
+        double distance = center.distanceTo(other.center);
+        return distance < radius + other.radius;
+    }
+
 }
--- Point2D.java
+++ Point2D.java
@@ -1,3 +1,35 @@
 public class Point2D {
-    
+    private double x;
+    private double y;
+
+    public Point2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double distanceTo(Point2D point) {
+        double xDifference = x - point.x;
+        double yDifference = y - point.y;
+        return Math.sqrt(xDifference * xDifference + yDifference * yDifference);
+    }
+
+    public String toString(){
+        return "(" + x + ", " + y + ")";
+    }
 }
